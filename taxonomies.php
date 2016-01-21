3a0d4081db783641ec4f46f05599103f3b065b1a
Stuff90 <bernard.simon.90@gmail.com>
2016-01-21 15:35:28 +0100

    master > Handle the i18n

--- taxonomies.php
+++ taxonomies.php
@@ -19,6 +19,40 @@
 
 	class Taxonomy
 	{
+        private $language = 'default';
+
+        private $i18n = array(
+            'default' => array(
+                'search'  => 'Search',
+                'all'     => 'All',
+                'parent'  => 'Parent',
+                'edit'    => 'Edit',
+                'update'  => 'Update',
+                'add-new' => 'Add new',
+                'new'     => 'New',
+                'name'    => 'Name'
+            )
+        );
+
+        public function setI18n( $language , $key , $value = null ) {
+            if( !array_key_exists($language , $this->i18n )) {
+                $this->i18n[$language] = $this->i18n['default'];
+            }
+
+            if( gettype($key) === 'string' && !is_null($value)) {
+                $this->i18n[$language][$key] = $value;
+            }
+
+            if ( gettype($key) === 'array') {
+                $this->i18n[$language] = array_merge( $this->i18n[$language] , $key );
+            }
+
+            return $this;
+        }
+
+        public function setLanguage( $theLanguage ) {
+            $this->language = $theLanguage;
+        }
 
 		public function setSlug( $slug )	 { $this->slug = $slug; return $this; }
 		public function setTarget( $target ) { $this->target = $target; return $this; }
@@ -62,29 +96,33 @@
 				throw new Exception('"isHierarchical" must be set ! Please use setIsHierarchical( $value );');
 				return;
 			}
+			if( !$this->i18n[$this->language] ){
+				throw new Exception('The laguage you defined has not been set');
+				return;
+			}
 
 
 			$args = array(
-				'hierarchical' 	=> $this->isHierarchical,
-				'labels' 		=> array(
-					'name' 				=> _x( $this->name, 'taxonomy general name' ),
-					'singular_name' 	=> _x( $this->singularName , 'taxonomy singular name' ),
-					'search_items' 		=>  __( isset($this->searchItem) ? $this->searchItem : 'Search ' . $this->name ),
-					'all_items' 		=> __( isset($this->allItem) ? $this->allItem : 'All ' . $this->name ),
-					'parent_item' 		=> __(  isset($this->parentItem) ? $this->parentItem : 'Parent ' . $this->singularName),
-					'parent_item_colon' => __(  isset($this->parentItem) ? $this->parentItem : 'Parent ' . $this->singularName ),
-					'edit_item' 		=> __(  isset($this->editItem) ? $this->editItem : 'Edit ' . $this->singularName  ),
-					'update_item' 		=> __(  isset($this->updateItem) ? $this->updateItem : 'Update ' . $this->singularName  ),
-					'add_new_item' 		=> __(  isset($this->addNewItem) ? $this->addNewItem : 'Add new ' . $this->singularName  ),
-					'new_item_name' 	=> __(  isset($this->newItemName) ? $this->newItemName : 'New ' . $this->singularName .' Name' ),
-					'menu_name' 		=> __(  isset($this->menuName) ? $this->menuName : $this->name  ),
-				),
-				'rewrite' => array(
-					'slug' 			=> isset($this->rewriteSlug) ? $this->rewriteSlug : $this->slug,
-					'with_front' 	=> isset($this->rewriteFront) ? $this->rewriteFront : false,
-					'hierarchical' 	=> $this->isHierarchical
-				)
-			);
+                'hierarchical'  => $this->isHierarchical,
+                'labels'        => array(
+                    'name'              => _x( $this->name, 'taxonomy general name' ),
+                    'singular_name'     => _x( $this->singularName , 'taxonomy singular name' ),
+                    'search_items'      =>  __( isset($this->searchItem) ? $this->searchItem : $this->i18n[$this->language]['search'] . ' ' . $this->name ),
+                    'all_items'         => __( isset($this->allItem) ? $this->allItem : $this->i18n[$this->language]['all'] .' ' . $this->name ),
+                    'parent_item'       => __(  isset($this->parentItem) ? $this->parentItem : $this->i18n[$this->language]['parent'] . ' ' . $this->singularName),
+                    'parent_item_colon' => __(  isset($this->parentItem) ? $this->parentItem : $this->i18n[$this->language]['parent'] . ' ' . $this->singularName ),
+                    'edit_item'         => __(  isset($this->editItem) ? $this->editItem : $this->i18n[$this->language]['edit'] . ' ' . $this->singularName  ),
+                    'update_item'       => __(  isset($this->updateItem) ? $this->updateItem : $this->i18n[$this->language]['update'] . ' ' . $this->singularName  ),
+                    'add_new_item'      => __(  isset($this->addNewItem) ? $this->addNewItem : $this->i18n[$this->language]['add-new'] . ' ' . $this->singularName  ),
+                    'new_item_name'     => __(  isset($this->newItemName) ? $this->newItemName : $this->i18n[$this->language]['new'] . ' ' . $this->singularName . ' ' . $this->i18n[$this->language]['name'] ),
+                    'menu_name'         => __(  isset($this->menuName) ? $this->menuName : $this->name  ),
+                ),
+                'rewrite' => array(
+                    'slug'          => isset($this->rewriteSlug) ? $this->rewriteSlug : $this->slug,
+                    'with_front'    => isset($this->rewriteFront) ? $this->rewriteFront : false,
+                    'hierarchical'  => $this->isHierarchical
+                )
+            );
 
 			register_taxonomy($this->slug, $this->target , $args);
 		}
